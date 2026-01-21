6de6ccccdc72977c23e0d84ce4bb6f6b3aa00cbe
AndyDough <andynguyendo@gmail.com>
2026-01-20 16:49:11 -0800

    add markdown view plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return { {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+  opts = {},
+} }
